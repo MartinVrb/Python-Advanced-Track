8c79f10ce37e4a3286f30bcc3ed18234234844e9
Martin Varbanov <148352793+MartinVrb@users.noreply.github.com>
2024-11-12 01:00:53 +0200

    Add files via upload

---
+++ 01_python_basics/01_first_steps_in_coding_py/02_first_steps_in_coding_py_exercise/08_basketball_equipment.py
@@ -0,0 +1,9 @@
+annual_fee_basketball = int(input())
+basketball_shoes = annual_fee_basketball * 0.60
+basketball_outfit = basketball_shoes * 0.80
+basketball_ball = basketball_outfit * 0.25
+basketball_accessories = basketball_ball * 0.20
+
+total_price = annual_fee_basketball + basketball_shoes + basketball_outfit + basketball_ball + basketball_accessories
+
+print(total_price)
