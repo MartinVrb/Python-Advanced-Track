3b15ddb9324eff40274bb017c64f3133e7fd3303
Martin Varbanov <148352793+MartinVrb@users.noreply.github.com>
2024-12-04 00:33:53 +0200

    Add files via upload

---
+++ 01_python_basics/05_while_loop_py/02_while_loop_py_ex/06_cake.py
@@ -0,0 +1,14 @@
+width = int(input())
+length = int(input())
+pieces = width * length
+
+while True:
+    command = input()
+    if command == "STOP":
+        print(f"{pieces} pieces are left.")
+        break
+    command = int(command)
+    pieces -= command
+    if pieces < 0:
+        print(f"No more cake left! You need {abs(pieces)} pieces more.")
+        break
